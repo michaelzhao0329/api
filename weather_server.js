6b2b1026c168ee49c9a4d430033dcea364ff984c
michaelzhao0329 <60829192+michaelzhao0329@users.noreply.github.com>
2020-03-28 20:38:13 -0400

    Add files via upload

---
+++ weather_server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const bodyParser=require("body-parser");
+const https=require("https");
+const request=require("request");
+
+const app = express();
+const port = 4000;
+const key="2586bbb8f6c285d7841e7a43148b107d";
+//var url="https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2586bbb8f6c285d7841e7a43148b107d";
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use("/",  express.static(__dirname + '/public'));
+//app.use("/styles",  express.static(__dirname + '/public/stylesheets'));
+app.use("/scripts", express.static(__dirname + '/public/javascripts'));
+app.use("/images",  express.static(__dirname + '/public/images'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+"/public/index.html");
+});
+
+app.get('/again', function (req, res) {
+  res.sendFile(__dirname+"/public/index.html");
+});
+
+app.post('/go', function (req, res) {
+  console.log(req.body);
+  var city = req.body.city;
+  var url="https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=metric&APPID=" + key;
+
+  if ( city === "" ) {
+    res.sendFile(__dirname+"/public/index.html");
+  } else {
+
+
+  https.get(url,function(response){
+    //console.log(response);
+
+    response.on("data",function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const icon = weatherData.weather[0].icon;
+      const weatherDescription = weatherData.weather[0].description;
+      const humidity = weatherData.main.humidity;
+      const temp_min = weatherData.main.temp_min;
+      const temp_max = weatherData.main.temp_max;
+      const img="http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      const timezone = weatherData.timezone;
+
+      res.write("<head>");
+      res.write("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>");
+      res.write("<link rel='stylesheet' type='text/css' href='/stylesheets/styles.css'>");
+      res.write("</head>");
+      res.write("<body>");
+    	res.write("<div id='weather'>");
+      res.write("<p><h3>The weather of " + city + " is currently</h3></p>");
+      res.write("<p>" + weatherDescription + "<img src=" + img + " align='middle'>" + "</p>");
+      res.write("<p>Temperature is " + temp +" degree Celsius </p>");
+      res.write("<p>Humidity is " + humidity +"%</p>");
+      res.write("<p>Today's lowest temperature is " + temp_min +" degree Celsius </p>");
+      res.write("<p>Today's highest temperature is " + temp_max +" degree Celsius </p>");
+
+      res.write("<form action='/again' method='GET'> ");
+      res.write("<button class='btn btn-primary' type='submit' name='submit' >Lookup another City</button> ");
+      res.write("</form>");
+      res.write("</div>");
+      res.write("</body>");
+      res.send();
+      res.end();
+    });
+  });
+}
+});
+
+app.post('/failure', function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
